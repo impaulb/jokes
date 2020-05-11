8c89fc62b95ad5198cb990954ece64bf86a7af2f
Paul <59670248+impaulb@users.noreply.github.com>
2020-05-11 12:16:20 -0700

    Updates

---
+++ precache-manifest.d9588b25d47acc2e9f1791a23ccaefec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06afac07173f13870f7b21ca0e61d027",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "22342b3275e2bc90a96e",
+    "url": "/jokes/static/css/main.e1694274.chunk.css"
+  },
+  {
+    "revision": "4cea5444f635fe52809f",
+    "url": "/jokes/static/js/2.56eccd33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokes/static/js/2.56eccd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22342b3275e2bc90a96e",
+    "url": "/jokes/static/js/main.70cad7fe.chunk.js"
+  },
+  {
+    "revision": "d95be01f0e79f66c8cf5",
+    "url": "/jokes/static/js/runtime-main.3a757f36.js"
+  }
+]);
